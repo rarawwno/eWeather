6143a5add1bddb7f43e1b253e5458a729c2477b8
AD_YE <1986322362@qq.com>
2023-11-26 12:21:43 +0800

    整理

--- app/src/main/java/com/example/util/ControlManager.java
+++ app/src/main/java/com/example/util/ControlManager.java
@@ -1,19 +1,10 @@
 package com.example.util;
 
 import com.example.listener.ClickListener;
-import com.example.listener.OptionItemSelectListener;
 
 public class ControlManager {
     ClickListener clickListener=new ClickListener();
-    OptionItemSelectListener optionItemSelectListener=new OptionItemSelectListener();
 
-    public OptionItemSelectListener getOptionItemSelectListener() {
-        return optionItemSelectListener;
-    }
-
-    public void setOptionItemSelectListener(OptionItemSelectListener optionItemSelectListener) {
-        this.optionItemSelectListener = optionItemSelectListener;
-    }
 
     public ClickListener getClickListener() {
         return clickListener;
